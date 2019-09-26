ad26b5f3929111e840e4b3e0b33f9897843b903f
Dominiccastaneda <dominiccastaneda@gmail.com>
2019-09-26 03:23:47 +0000

    now

--- Homework5/register.php
+++ Homework5/register.php
@@ -41,7 +41,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     <main>
         <h1>Sticky Form</h1>
 
-        <form action="login.php" method="POST">
+        <form action="register.php" method="POST">
             <label for="first_name">First Name</label>
             <input type="first_name" id="first_name" name="first_name" placeholder="" value="<?php echo $first_name ?>">
             <label for="last_name">Last Name</label>
@@ -53,7 +53,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
             <label for="confirm_password">Confirm Password</label>
             <input type="confirm_password" id="confirm_password" name="confirm_password" placeholder="" value="<?php echo $confirm_password ?>">
 
-            <button>Register!</button>
+            <input type="submit" name="submit" value="Submit"/>
     </main>
 
 <?php include('includes/footer.php'); ?>
