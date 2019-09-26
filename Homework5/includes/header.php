422bd09353542bc870bcdb6c44cc6d1f25a9949e
Dominiccastaneda <dominiccastaneda@gmail.com>
2019-09-26 02:23:09 +0000

    first

--- Homework5/includes/header.php
+++ Homework5/includes/header.php
@@ -4,6 +4,20 @@
          <meta charset="utf-8">
          <title>Homepage</title>
     <style>
+    html, body {
+	width: 100%;
+	height: 100%;
+	background-image: url(../images/nav2.png);
+	background-repeat: repeat-x;
+    }
+    #container {
+	width: 1000px;
+	margin-left: auto;
+	margin-right: auto;
+	margin-bottom: -60px;
+	min-height: 100%;
+	position: relative;
+    }
     #header {background-image:url(../images/nav.png); height: 100px; margin-top: -16px;}
     #nav li {list-style: none; float: right; margin-top: 58px;	padding-right: 60px; text-align: right;
 	font-family: Segoe, "Segoe UI", "DejaVu Sans", "Trebuchet MS", Verdana, sans-serif;}
