0c53789c75ff836a5582b7c64ee112bd8f67005a
Dominiccastaneda <dominiccastaneda@gmail.com>
2019-10-02 05:20:43 +0000

    footer

--- Homework5/includes/footer.php
+++ Homework5/includes/footer.php
@@ -1,6 +1,11 @@
 <!start of footer
 
-    <footer><p>Footer</p></footer>
+    <footer><p>Created by: <br>
+                Dominic Castaneda <br?
+                Gaith Cattan <br>
+                Rene Lopez <br>
+                Patrick Olesiak<br>
+                </p></footer>
 
 
 
