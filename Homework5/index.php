186441e60eda384dee0d7359936feb55a6cda0bd
Dominiccastaneda <38331931+Dominiccastaneda@users.noreply.github.com>
2019-10-02 18:06:54 -0700

    Add files via upload

--- Homework5/index.php
+++ Homework5/index.php
@@ -3,7 +3,17 @@
 <?php include('includes/header.php'); ?>
 
     <main>
-        <h1>This Is The Homepage</h1>
+        
+        <div id="container">
+        <div id="texting">
+
+
+        <h2 style="font-size: 4em;">MAIN CONTENT HOME PAGE</h2>      
+        <h2 style="font-size: 4em;">Join Now</h2>
+        <h2 style="font-family: 'Anton', sans-serif;">Do you wish to join our non-profit group and change the world.</h2>
+
+        </div>
+        </div>
 
     </main>
 
